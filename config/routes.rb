a7f5ef5d83dd5ee8dfb56971197c6d4b7c512465
Nestor <nestor.gomez@hoytrabajas.com>
2022-06-17 19:24:25 -0500

    about_us

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get "/about_us",  to: "about_us#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
